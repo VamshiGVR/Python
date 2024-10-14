3020e1293351ac3ca54c30f33a36ddc548154802
VamshiGVR <163581641+VamshiGVR@users.noreply.github.com>
2024-10-14 16:37:19 -0400

    functions (#23)
    Co-authored-by: gunvamshidharreddy <gunvamshidharreddy@gmail.com>

--- Projects/BlackJack_GAME/main.py
+++ Projects/BlackJack_GAME/main.py
@@ -1,3 +1,14 @@
 #This game is created for learning purpose
 
+<<<<<<< HEAD
+import random
+
+def deal_card():
+    
+    Cards = [11, 2,3,4,5,6,7,8,9,10,10,10,10]
+    randomCard = random.choice(Cards)
+    return randomCard
+
+=======
 Cards = [11, 2,3,4,5,6,7,8,9,10,10,10,10]
+>>>>>>> edbc4d41f522bd6e0b9992d2bb14a0c3e6e79579
